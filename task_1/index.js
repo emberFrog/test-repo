cf7ff45597375b31c0f19b5b27a384be6abb0d51
Nikita Polianskii <157965326+emberFrog@users.noreply.github.com>
2024-04-21 14:44:21 +0300

    complited-task_1

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,41 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from './data.js'
 
-console.log("Let's rock")
-console.log(encoded, translations)
+function decodeFields(encodedData) {
+	return encodedData.map(item => {
+		const decodedItem = {}
+		for (const key in item) {
+			if (key.endsWith('Id')) {
+				if (['groupId', 'service', 'formatSize', 'ca'].includes(key)) {
+					decodedItem[key] = item[key]
+				} else {
+					decodedItem[key] = translations[item[key]] || item[key]
+				}
+			} else {
+				decodedItem[key] = item[key]
+			}
+		}
+		return decodedItem
+	})
+}
 
+function collectUniqueIds(encodedData) {
+	const uniqueIds = new Set()
+	encodedData.forEach(item => {
+		for (const key in item) {
+			if (key.endsWith('Id')) {
+				uniqueIds.add(item[key])
+			}
+		}
+	})
+	return Array.from(uniqueIds)
+}
 
+const decodedData = decodeFields(encoded)
+const uniqueIdsList = collectUniqueIds(encoded)
 
+console.log('Decoded Data:', decodedData)
+console.log('Unique IDs:', uniqueIdsList)
+
+// console.log("Let's rock")
+// console.log(encoded, translations)
 // console.log(decoded)
