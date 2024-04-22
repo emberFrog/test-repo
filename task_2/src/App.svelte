8528029b60ccd894f9f02b4ffa43e0eb259df6ba
Nikita Polianskii <157965326+emberFrog@users.noreply.github.com>
2024-04-22 16:18:20 +0300

    main-logic

--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -1,49 +1,116 @@
 <script lang="ts">
-	import svelteLogo from './assets/svelte.svg'
-	import viteLogo from '/vite.svg'
-	import Counter from './lib/Counter.svelte'
+	let currencyIn: string = 'USD'
+	let currencyOut: string = 'RUB'
+	let amountIn: number = 0
+	let amountOut: number = 0
+	$: amountOut
+
+	const API_TOKEN = 'cdc4f94dac9c1e56f4a4060a'
+
+	async function convertCurrency(): Promise<void> {
+		if (!currencyIn || !currencyOut || !amountIn) return
+
+		try {
+			const response = await fetch(
+				`https://v6.exchangerate-api.com/v6/latest/${currencyIn}`,
+				{
+					headers: {
+						Authorization: `Bearer ${API_TOKEN}`,
+					},
+				}
+			)
+			const data: { conversion_rates: { [key: string]: number } } =
+				await response.json()
+			const rate = data.conversion_rates[currencyOut]
+			amountOut = amountIn * rate
+		} catch (error) {
+			console.error('Error: currency conversion', error)
+		}
+	}
 </script>
 
-<main>
-	<div>
-		<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-			<img src={viteLogo} class="logo" alt="Vite Logo" />
-		</a>
-		<a href="https://svelte.dev" target="_blank" rel="noreferrer">
-			<img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-		</a>
-	</div>
-	<h1>Vite + Svelte</h1>
+<main class="wrapper">
+	<header>
+		<h1>Currency converter app</h1>
+		<p>Created by Nikita Polianskii</p>
+	</header>
 
-	<div class="card">
-		<Counter />
-	</div>
-
-	<p>
-		Check out <a
-			href="https://github.com/sveltejs/kit#readme"
-			target="_blank"
-			rel="noreferrer">SvelteKit</a
-		>, the official Svelte app framework powered by Vite!
-	</p>
-
-	<p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+	<section class="inputSection">
+		<fieldset>
+			<label for="curr-from">FROM</label>
+			<input
+				id="curr-from"
+				name="curr-from"
+				type="text"
+				bind:value={currencyIn}
+				on:input={convertCurrency}
+			/>
+			<label for="curr-to">TO</label>
+			<input
+				id="curr-to"
+				name="curr-to"
+				type="text"
+				bind:value={currencyOut}
+				on:input={convertCurrency}
+			/>
+		</fieldset>
+		<fieldset>
+			<label for="amount">AMOUNT</label>
+			<input
+				id="amount"
+				name="amount"
+				type="number"
+				min="0"
+				bind:value={amountIn}
+				on:input={convertCurrency}
+			/>
+			<label for="converted">CONVERTED</label>
+			<input
+				id="converted"
+				name="converted"
+				type="number"
+				bind:value={amountOut}
+			/>
+		</fieldset>
+	</section>
 </main>
 
 <style>
-	.logo {
-		height: 6em;
-		padding: 1.5em;
-		will-change: filter;
-		transition: filter 300ms;
+	.wrapper {
+		background-color: #1b1b32;
+		color: #f5f6f7;
+		border-radius: 20px;
+		font-family: Tahoma;
+		font-size: 16px;
+		width: 100%;
+		height: 100vh;
 	}
-	.logo:hover {
-		filter: drop-shadow(0 0 2em #646cffaa);
+
+	header {
+		padding: 40px;
 	}
-	.logo.svelte:hover {
-		filter: drop-shadow(0 0 2em #ff3e00aa);
+
+	h1 {
+		margin: 1em auto;
+		text-align: center;
 	}
-	.read-the-docs {
-		color: #888;
+
+	fieldset {
+		border: none;
+		padding: 1rem 0;
+	}
+
+	label {
+		display: block;
+		margin: 0.5rem 0;
+	}
+
+	input {
+		margin: 0;
+		width: 60%;
+		min-height: 2em;
+		background-color: #0a0a23;
+		border: 1px solid #0a0a23;
+		color: #ffffff;
 	}
 </style>
